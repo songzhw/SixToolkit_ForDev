ba6ab7165c77a9484ca829d4931bc6f4bd712549
songzhw <songzhw2012@gmail.com>
2016-05-14 00:03:36 -0400

    finish the auto_rv.groovy

--- auto-recycler-view/auto_rv.groovy
+++ auto-recycler-view/auto_rv.groovy
@@ -6,16 +6,21 @@ import java.text.DateFormat
  * Created by songzhw on 2016-05-13.
  */
 
+// [input from users]
 packageName = 'ca.six.todo'
-basicName = 'Second'
+basicName = 'Second' // ***Adapter
+itemType = 'String' // the class type of data
+layoutName = 'item_home'  // inflate R.layout.***
 
+// ============================================================
+// [code begins]
 adapterName = "${basicName}Adapter"
-adapterFileName = "${adapterName}.java"
+resultFileName = "${adapterName}.java"
 lineSeparator = System.getProperty('line.separator')
 
 sb = new StringBuilder()
 generateAdapterContent()
-write2File(adapterFileName, sb)
+write2File(resultFileName, sb)
 
 def write2File(fileFullName, content){
     def file = new File(fileFullName)
@@ -38,15 +43,47 @@ def generateAdapterContent() {
 
 
     sb<<"// Created by songzhw on ${new Date().format( 'yyyy/MM/dd')}"<<lineSeparator
-    sb<<""<<lineSeparator
-    sb<<""<<lineSeparator
-    sb<<""<<lineSeparator
-    sb<<""<<lineSeparator
-    sb<<""<<lineSeparator
+    sb<<"public class ${adapterName} extends RecyclerView.Adapter<${adapterName}.ViewHolder> {"<<lineSeparator
+    sb<<"\tpublic List<$itemType> data;"<<lineSeparator
+    sb<<lineSeparator
+
+    sb<<"\t@Override"<<lineSeparator
+    sb<<"\tpublic ViewHolder onCreateViewHolder(ViewGroup parent, int viewType) {"<<lineSeparator
+    sb<<"\t\tView itemView = LayoutInflater.from(parent.getContext()).inflate(R.layout.${layoutName}, null);"<<lineSeparator
+    sb<<"\t\tViewHolder holder = new ViewHolder(itemView);"<<lineSeparator
+    sb<<"\t\treturn holder;"<<lineSeparator
+    sb<<"\t}"<<lineSeparator
+    sb<<lineSeparator
 
 
+    sb<<"\t@Override"<<lineSeparator
+    sb<<"\tpublic void onBindViewHolder(ViewHolder holder, int position) {"<<lineSeparator
+    sb<<"\t\tif(data != null && data.size() > position){"<<lineSeparator
+    sb<<"\t\t\t${itemType} item = data.get(position);"<<lineSeparator
+    sb<<"\t\t\t// holder.tv.setText(item.text);"<<lineSeparator
+    sb<<"\t\t\t// holder.tv.setText(item.text);"<<lineSeparator
+    sb<<"\t\t}"<<lineSeparator
+    sb<<"\t}"<<lineSeparator
+    sb<<lineSeparator
 
-    sb<<""<<lineSeparator
-    sb<<""<<lineSeparator
-    sb<<""<<lineSeparator
+    sb<<"\t@Override"<<lineSeparator
+    sb<<"\tpublic int getItemCount() {"<<lineSeparator
+    sb<<"\t\treturn data == null ? 0 : data.size();"<<lineSeparator
+    sb<<"\t}"<<lineSeparator
+    sb<<lineSeparator
+
+
+    sb<<lineSeparator
+    sb<<"\tpublic class ViewHolder extends RecyclerView.ViewHolder {"<<lineSeparator
+    sb<<"\t\t // public TextView tv;"<<lineSeparator
+    sb<<lineSeparator
+    sb<<"\t\tpublic ViewHoler(View itemView) {"<<lineSeparator
+    sb<<"\t\t\tsuper(itemView);"<<lineSeparator
+    sb<<"\t\t\t// tv = (TextView)itemView.findViewById(R.id.tvTask);"<<lineSeparator
+    sb<<"\t\t}"<<lineSeparator
+    sb<<"\t}"<<lineSeparator
+    sb<<lineSeparator
+    sb<<lineSeparator
+
+    sb<<"}"<<lineSeparator
 }
