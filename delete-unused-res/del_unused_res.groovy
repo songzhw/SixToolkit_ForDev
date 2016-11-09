07645fe46eb689ade98200aa2b3ffcc1dcef34e9
songzhw <songzhw2012@gmail.com>
2016-11-09 05:59:37 -0500

    del_unused_res.groovy

--- delete-unused-res/del_unused_res.groovy
+++ delete-unused-res/del_unused_res.groovy
@@ -1,10 +1,26 @@
+
+
 // ===================== 1. set basic argumetns =====================
 projectPath = 'E:/workspace/mine/SixTools'
 
 
 // ===================== 2. main entrance =====================
-def commandLint = "cmd /c gradlew.bat :app:lintDebug > a.txt" //TODO mac is different
-commandLint.execute(null, new File(projectPath))
+// TODO mac is different
+// TODO "app" module can be changed
+
+// def commandLint = "cmd /c gradlew.bat :app:lintDebug > a.txt" 
+// commandLint.execute(null, new File(projectPath))
+
+
+lintResultXmlFile = "C:/Users/songz2/temp/lint-results-temp.xml"
+
+def axml = new XmlParser().parse(lintResultXmlFile)
+println "lint.by = ${axml.attribute("by")}"
+
+axml.issue.each {
+    println it.attribute("id")
+}
+
 
 
 // ===================== 3. run lint =====================
