c052ff1151499ab51c81af1adfcb33d428fb3521
songzhw <songzhw2012@gmail.com>
2016-12-07 21:59:11 -0500

    tool

--- json-gson-model/auto_model.groovy
+++ json-gson-model/auto_model.groovy
@@ -10,9 +10,29 @@ ajson = new JsonSlurper().parse(reader)
 
 ajson.each { key, value ->
     println "$key (type = ${value.getClass()})"
+    println "TYPE : ${getType(key, value)}"
+    println "-----------------------------------------------------------------"
 }
 
 
 def getType(key, value){
-    
+    valueType = value.getClass()
+    switch(valueType){
+        case 'class java.lang.String':
+            return 'String';
+        case 'class java.lang.Boolean':
+            return 'boolean';
+        case 'class java.lang.Integer':
+            return 'int';
+        case 'class java.util.ArrayList':
+            listSubClass = value[0].getClass()
+            //println "getType() : ArrayList : listSubClass = $listSubClass"
+            listSubType = getTypeFromWholePath("${key}Item", value[0])
+            return "ArrayList<$listSubType>"
+        case 'class groovy.json.internal.LazyMap':
+            //println "$key -- ${key.getClass()}"
+            objectType = key.capitalize();
+            return objectType;
+    //return 'Object'
+    }
 }
