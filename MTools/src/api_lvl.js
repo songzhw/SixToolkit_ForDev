2125d44a9fcbfcff6a7d2049f20671153fe5fe42
songzhw <songzhw2012@gmail.com>
2019-11-22 21:35:24 -0500

    render a grid

--- MTools/src/api_lvl.js
+++ MTools/src/api_lvl.js
@@ -1,5 +1,15 @@
+const Grid = require("console-grid");
+const grid = new Grid();
+
 function getApi(num) {
-  console.log(`search api ${num}`);
+  const data = {
+    columns: [{ id: "id", name: "api level" }, { id: "name", name: "name" }, { id: "version", name: "version" }, {id:"date", name:"release @"}],
+    rows: [{ id: num, name: "hello", version: "v1.0" },
+      { id: 22, name: "xx", version: "v1.1" },
+      { id: 33, name: "oo", version: "v2.0" }
+    ]
+  };
+  grid.render(data);
 }
 
 module.exports = { getApi };
