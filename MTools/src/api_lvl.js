e6b2a32d1d95b009fc2f99191f946040d905da8f
songzhw <songzhw2012@gmail.com>
2019-11-22 21:48:44 -0500

    add colors

--- MTools/src/api_lvl.js
+++ MTools/src/api_lvl.js
@@ -1,10 +1,14 @@
+const androids = require("./res/ApiLevel");
+
 const Grid = require("console-grid");
+const Colors = Grid.Style;
 const grid = new Grid();
 
 function getApi(num) {
   const data = {
-    columns: [{ id: "id", name: "api level" }, { id: "name", name: "name" }, { id: "version", name: "version" }, {id:"date", name:"release @"}],
-    rows: [{ id: num, name: "hello", version: "v1.0" },
+    columns: [{ id: "id", name: "api level" }, { id: "name", name: "name" },
+      { id: "version", name: "version" }, { id: "date", name: "release @" }],
+    rows: [{ id: num, name: "i'm" + Colors.bg.green("hello"), version: "v1.0" },
       { id: 22, name: "xx", version: "v1.1" },
       { id: 33, name: "oo", version: "v2.0" }
     ]
