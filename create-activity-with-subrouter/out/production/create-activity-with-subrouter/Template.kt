7a9171b66216be0bb7cf0c49293131f9d4cc63b4
songzhw <songzhw2012@gmail.com>
2022-01-12 16:05:29 -0500

    read arg

---
+++ create-activity-with-subrouter/out/production/create-activity-with-subrouter/Template.kt
@@ -0,0 +1,6 @@
+class **ACTIVITY_NAME**Activity : BaseActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.**LAYOUT_NAME**)
+    }
+}
--- create-activity-with-subrouter/src/ca/six/tools/creator.groovy
+++ create-activity-with-subrouter/src/ca/six/tools/creator.groovy
@@ -2,3 +2,5 @@ package ca.six.tools
 
 def file = new File(".") // 当前目录 (而不是proj根目录)
 println file.absolutePath
+
+println this.args
