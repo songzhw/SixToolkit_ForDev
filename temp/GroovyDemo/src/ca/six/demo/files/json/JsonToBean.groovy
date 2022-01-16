bbd8c14807b595af4567ce96b2a81b5e084ab180
songzhw <songzhw2012@gmail.com>
2022-01-16 11:55:57 -0500

    json to bean

---
+++ temp/GroovyDemo/src/ca/six/demo/files/json/JsonToBean.groovy
@@ -0,0 +1,17 @@
+package ca.six.demo.files.json
+
+// 要引入GSON才有那个JSONObject类.
+// 我现在没引入, 所会不会成功
+
+/*
+class OneBean {
+    int id
+    String name
+    ArrayList<Integer> bags
+}
+
+Gson gson = new Gson()
+def reader = new FileReader("../../../../../../res/simple.json")
+println gson.fromJson(reader, OneBean)
+
+*/
